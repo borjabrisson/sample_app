a1fdd2f6486fe92d740c04694c93b2b7307e2cc7
Borja Brisson <invitado@perdidita.(none)>
2014-04-27 16:54:24 +0100

    Web Interface for following users END

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,8 +16,8 @@ describe "StaticPages" do
     describe "for signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        FactoryGirl.create(:micropost, user: user, content: "Lorem")
+        FactoryGirl.create(:micropost, user: user, content: "Ipsum")
         sign_in user
         visit root_path
       end
@@ -27,21 +27,31 @@ describe "StaticPages" do
           expect(page).to have_selector("li##{item.id}", text: item.content)
         end
       end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
     end
 
-  end
-
-  describe "Help page" do
+    describe "Help page" do
     before { visit help_path }
     it { should have_content('Help') }
     it { should have_title(full_title('Help')) }
   end
 
-  describe "About page" do
+    describe "About page" do
     before { visit about_path }
     it { should have_content('About') }
 #  it { should have_title("#{ base_title } | About") }
     it { should have_title(full_title('About')) }
   end
+ end
 
 end
